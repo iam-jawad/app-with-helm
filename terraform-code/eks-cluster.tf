4742cf029e49824d11a746488e2876f27e490015
jawad57903 <muhammad.jawad09@gmail.com>
2024-08-04 02:02:17 +0500

    created eks cluster

---
+++ terraform-code/eks-cluster.tf
@@ -0,0 +1,21 @@
+resource "aws_eks_cluster" "cluster" {
+  name     = "${var.cluster_name}-cluster"
+  version  = var.cluster_version
+  role_arn = aws_iam_role.eks-cluster-role.arn
+
+  vpc_config {
+
+    endpoint_private_access = false
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+
+    subnet_ids = [
+      aws_subnet.public-us-east-1a.id,
+      aws_subnet.private-us-east-1b.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.amazon-eks-cluster-policy]
+}
+
+
