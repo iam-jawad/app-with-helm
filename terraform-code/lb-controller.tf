d91761f50d29667751182736849174adad85457d
jawad57903 <muhammad.jawad09@gmail.com>
2024-08-05 02:14:47 +0500

    created lb controller related files

---
+++ terraform-code/lb-controller.tf
@@ -0,0 +1,46 @@
+resource "helm_release" "lb-controller" {
+  name = "lb-controller"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.4.1"
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.cluster.id
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v2.4.2"
+  }
+
+  set {
+    name  = "replicaCount"
+    value = 1
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.lb-controller-role.arn
+  }
+
+  # EKS Fargate specific
+  set {
+    name  = "region"
+    value = "us-east-1"
+  }
+
+  set {
+    name  = "vpcId"
+    value = aws_vpc.demo-vpc.id
+  }
+
+  depends_on = [aws_eks_fargate_profile.kubesystem-profile]
+}
